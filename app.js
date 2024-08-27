f94d6416d3e6c9fd225c3aad74349533f0174111
Ricky Chris <rickychris6@gmail.com>
2024-08-27 07:42:01 +0400

    add add remove feat

--- app.js
+++ app.js
@@ -1,22 +1,120 @@
-// const getNotes = require('./notes');
-import { getNotes } from './notes.js';
-import chalk from 'chalk'; 
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const notes = require('./notes.js')
 
-console.log(getNotes());
-console.log(chalk.green.inverse.bold("Success!"));
-console.log(chalk.red.inverse.bold("Error!"));
+// Create add command
+yargs(hideBin(process.argv))
+    .command({
+        command: 'add',
+        describe: 'Add a new note',
+        builder: {
+            title: {
+                describe: 'Note title',
+                demandOption: true,
+                type: 'string'
+            },
+            body: {
+                describe: 'Note body',
+                demandOption: true,
+                type: 'string'
+            }
+        },
+        handler: (argv) => {
+            notes.addNote(argv.title, argv.body);
+        }
+    })
+    .command({
+        command: 'remove',
+        describe: 'Remove a note',
+        builder: {
+            title: {
+                describe: 'Note title',
+                demandOption: true,
+                type: 'string'
+            }
+        },
+        handler: (argv) => {
+            console.log('Removing a note : ', argv.title);
+            notes.removeNote(argv.title);
+        }
+    })
+    .command({
+        command: 'list',
+        describe: 'List all notes',
+        handler: (argv) => {
+            console.log('Listing all notes');
+        }
+    })
+    .command({
+        command: 'read',
+        describe: 'Read a note',
+        handler: (argv) => {
+            console.log('Reading a note');
+        }
+    })
+    .parse()
+    
+// Create a remove command
+// yargs(hideBin(process.argv))
+//     .command({
+//         command: 'remove',
+//         describe: 'Remove a note',
+//         handler: (argv) => {
+//             console.log('Removing a note');
+//         }
+//     })
+//     .parse()
+// // const getNotes = require('./notes');
+// import { getNotes } from './notes.js';
+// import chalk from 'chalk'; 
+// import yargs from 'yargs/yargs';
+// import { hideBin } from 'yargs/helpers'
+
+// const yrgs = yargs(process.argv);
+// yrgs.version('1.1.0').alias('v', 'version');
+
+// console.log(yrgs.argv);  
+
+// yrgs.command('add', 'Add a new note', (yargs) => {
+//     console.log('Adding a new note');
+// })
+// yargs(hideBin(process.argv))
+//   .command('serve [port]', 'start the server', (yargs) => {
+//     return yargs
+//       .positional('port', {
+//         describe: 'port to bind on',
+//         default: 5000
+//       })
+//   }, (argv) => {
+//     if (argv.verbose) console.info(`start server on :${argv.port}`)
+//     serve(argv.port)
+//   })
+//   .option('verbose', {
+//     alias: 'v',
+//     type: 'boolean',
+//     description: 'Run with verbose logging'
+//   })
+//   .parse()
+
+// const command = process.argv[2];
+
+// if (command === 'add') {
+//     console.log('Adding note');
+// } else if (command === 'remove') {
+//     console.log('Removing note');
+// }
 
 
-// const fs = require('fs');
-// const readline = require('node:readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+// // const fs = require('fs');
+// // const readline = require('node:readline');
+// // const rl = readline.createInterface({
+// //   input: process.stdin,
+// //   output: process.stdout,
+// // });
 
-// rl.question(`Enter any text:\n`, text => {
-//     fs.appendFileSync('notes.txt', '\n' + text);
-//     console.log("Text appended to file successfully");
-//     rl.close();
-//   });
-// fs.writeFileSync('notes.txt', 'Hello World!');
+// // rl.question(`Enter any text:\n`, text => {
+// //     fs.appendFileSync('notes.txt', '\n' + text);
+// //     console.log("Text appended to file successfully");
+// //     rl.close();
+// //   });
+// // fs.writeFileSync('notes.txt', 'Hello World!');
--- notes.js
+++ notes.js
@@ -1,9 +1,56 @@
-import validator from 'validator'
-// const validator = require('validator')
-export const getNotes = () => {
+const fs = require('fs');
+const chalk = require('chalk');
+
+const getNotes = () => {
     const message = "Your notes ...";
     console.log(validator.isEmail('a@s.com'))
 
     return message
 }
-// module.exports.getNotes = getNotes;
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log("New note added!")
+    } else {
+        console.log("Note title taken!")
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title)
+    if (notes.length > notesToKeep.length) {
+        saveNotes(notesToKeep)
+        console.log(chalk.green.inverse("Note removed!"))
+        // console.log("Note removed!")
+    } else {
+        // console.log("No note found!")
+        console.log(chalk.red.inverse("No note found!"))
+    }
+}
+
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes.json', JSON.stringify(notes))
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuff = fs.readFileSync('notes.json')
+        const data = JSON.parse(dataBuff.toString());
+        return data
+    } catch (e) {
+        return []
+    }
+}
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
